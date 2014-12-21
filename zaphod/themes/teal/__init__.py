266c0c64b592e857a3074316be9fcf40a8037051
Scott Torborg <scott@cartlogic.com>
2014-12-20 17:18:57 -0800

    Add frontend structure to theme, including bootstrap 3 LESS, javascript, base template, and favicon

--- zaphod/themes/teal/__init__.py
+++ zaphod/themes/teal/__init__.py
@@ -1,5 +1,23 @@
 from pyramid_frontend.theme import Theme
+from pyramid_frontend.images import FilterChain
+from pyramid_frontend.assets.less import LessAsset
+from pyramid_frontend.assets.requirejs import RequireJSAsset
 
 
 class TealTheme(Theme):
     key = 'teal'
+
+    assets = {
+        'main-less': LessAsset(
+            '/_teal/css/main.less',
+            less_path='/_teal/js/vendor/less.js',
+            lessc_path='/var/sw/less-1.7.0/node_modules/less/bin/lessc',
+        ),
+        'main-js': RequireJSAsset(
+            '/_teal/js/main.js',
+            require_config_path='/_teal/js/require_config.js',
+            require_base_url='/_teal/js/vendor/',
+        ),
+    }
+
+

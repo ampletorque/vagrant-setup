547aa0af489b790ab515c02b9b64c478c5d816cd
Scott Torborg <scott@cartlogic.com>
2015-01-04 19:43:37 -0800

    Flesh out styleguide markup more

--- zaphod/views/styleguide.py
+++ zaphod/views/styleguide.py
@@ -1,9 +1,12 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+from formencode import Schema
+from pyramid_uniform import Form, FormRenderer
 from pyramid.view import view_config
 
 
 @view_config(route_name='styleguide', renderer='styleguide.html')
 def styleguide_view(request):
-    return {}
+    form = Form(request, schema=Schema())
+    return dict(renderer=FormRenderer(form))

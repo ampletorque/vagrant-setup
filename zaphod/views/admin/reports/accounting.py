d2ebe2b6e22e6f189278db53e5f90d2c740896f2
Scott Torborg <scott@cartlogic.com>
2015-02-16 01:05:35 -0800

    Clean up admin interface a bit and add scaffold templates for reporting

--- zaphod/views/admin/reports/accounting.py
+++ zaphod/views/admin/reports/accounting.py
@@ -11,17 +11,38 @@ class AccountingReportsView(BaseReportsView):
                  renderer='admin/reports/revenue.html',
                  permission='authenticated')
     def revenue(self):
+        # for a time range
+
         # crowdfunding fees, recognized at the time the project is successful
+        # XXX
+        crowdfunding_fees = 0
 
         # preorder commission, recognized at the time of order??
+        # XXX
+        preorder_fees = 0
 
         # in-stock sales, recognized at the time of shipment
+        # XXX
+        stock_items = 0
 
         # shipping revenue, recognized at the time of shipment
+        # XXX
+        stock_shipping = 0
 
         # fulfillment fees, recognized at the time of shipment
+        # XXX
+        fulfillment_fees = 0
+
+        total = (crowdfunding_fees + fulfillment_fees + preorder_fees +
+                 stock_items + stock_shipping)
 
         return {
+            'crowdfunding_fees': crowdfunding_fees,
+            'fulfillment_fees': fulfillment_fees,
+            'preorder_fees': preorder_fees,
+            'stock_items': stock_items,
+            'stock_shipping': stock_shipping,
+            'total': total,
         }
 
     @view_config(route_name='admin:reports:cogs',
@@ -31,10 +52,19 @@ class AccountingReportsView(BaseReportsView):
         # for a time range
 
         # in-stock product cost
-        # fulfillment cost
+        # XXX
+        stock_items = 0
+
         # outbound freight cost
+        # XXX
+        shipping = 0
+
+        total = stock_items + shipping
 
         return {
+            'stock_items': stock_items,
+            'shipping': shipping,
+            'total': total,
         }
 
     @view_config(route_name='admin:reports:inventory',

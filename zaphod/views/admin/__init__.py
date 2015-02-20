c757ec1a102c8186829d739ec96553b84ab5ab1c
Scott Torborg <scott@cartlogic.com>
2015-02-20 08:09:09 -0800

    Gradual progress on project admin interface

--- zaphod/views/admin/__init__.py
+++ zaphod/views/admin/__init__.py
@@ -22,6 +22,7 @@ def includeme(config):
     config.add_route('admin:project:reports:status', '/project/{id}/status')
     config.add_route('admin:project:reports:balance', '/project/{id}/balance')
     config.add_route('admin:project:reports:skus', '/project/{id}/skus')
+    config.add_route('admin:product', '/product/{id}')
     config.add_route('admin:update', '/update/{id}')
 
     config.add_route('admin:users', '/users')
---
+++ zaphod/views/admin/products.py
@@ -0,0 +1,27 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from pyramid.view import view_defaults
+from venusian import lift
+from formencode import Schema, validators
+
+from ... import model
+
+from .base import BaseEditView
+
+
+@view_defaults(route_name='admin:product', renderer='admin/product.html')
+@lift()
+class ProductEditView(BaseEditView):
+    cls = model.Product
+
+    class UpdateForm(Schema):
+        allow_extra_fields = False
+        name = validators.UnicodeString(not_empty=True)
+        international_available = validators.Bool()
+        international_surcharge = validators.Number()
+        gravity = validators.Int()
+        non_physical = validators.Bool()
+        published = validators.Bool()
+        price = validators.Number()
+        accepts_preorders = validators.Bool()

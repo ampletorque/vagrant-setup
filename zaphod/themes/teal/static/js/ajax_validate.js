3205dd1ccc058d239dc8efda466ca5111dc3c704
Scott Torborg <scott@cartlogic.com>
2015-03-08 22:45:33 -0700

    Add javascript functionality to do pjax-style form validation error rendering

--- zaphod/themes/teal/static/js/ajax_validate.js
+++ zaphod/themes/teal/static/js/ajax_validate.js
@@ -3,27 +3,51 @@ define(['jquery'], function ($) {
   // AJAX form validation.
   "use strict";
 
+  function clearErrors($form) {
+    $form.find('.form-group').removeClass('has-error').find('ul.error').remove();
+  }
+
+  function renderErrors($form, errors) {
+    $.each(errors, function(name, error) {
+      var
+        $input = $form
+          .find('input, select, textarea')
+          .filter('[name=' + name + ']'),
+        $group = $input.closest('.form-group');
+      $group.addClass('has-error');
+      $group.find('> div').append('<ul class="error"><li>' + error + '</li></ul>');
+    });
+  }
+
   function submitHandler(e) {
     e.stopPropagation();
     e.preventDefault();
 
     var $form = $(this);
+    console.log("form is", $form);
 
-    // Serialize the form data
-
-    // Make an ajax POST request to the form action
-
-    // Check status
-
-    // If status is ok, redirect to supplied location
-
-    // If status is fail, collect errors from response and render them
-
+    // Submit the form with an ajax request
+    $.ajax({
+      type: $form.attr('method'),
+      url: $form.attr('action'),
+      data: $form.serialize(),
+      dataType: 'json',
+      error: function (request, status, error) {
+        alert(request.responseText);
+      },
+      success: function (data, status, xhr) {
+        if(data.status === 'ok') {
+          window.location.replace(data.location);
+        } else {
+          clearErrors($form);
+          renderErrors($form, data.errors);
+        }
+      }
+    });
   }
 
   $(function () {
-    console.log("initialized");
-    //$('.js-ajax-validate').on('submit', submitHandler);
+    $('.js-ajax-validate').on('submit', submitHandler);
   });
 
 });

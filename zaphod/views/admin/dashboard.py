62990f907c88bc0647cd747bd68dc543f71094e7
Scott Torborg <scott@cartlogic.com>
2015-03-11 16:34:48 -0700

    Add today sales and order count to admin dashbaord metrics

--- zaphod/views/admin/dashboard.py
+++ zaphod/views/admin/dashboard.py
@@ -1,7 +1,9 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-from datetime import datetime
+import pytz
+from datetime import datetime, time
+from sqlalchemy.sql import func
 
 from pyramid.view import view_config
 
@@ -32,9 +34,29 @@ class DashboardView(object):
 
         user_q = model.Session.query(model.User)
 
+        utcnow = datetime.utcnow()
+        report_tz = pytz.timezone('America/Los_Angeles')
+        now_local = pytz.utc.localize(utcnow).astimezone(report_tz)
+        today_start_local = datetime.combine(now_local.date(), time())
+        today_start_utc = report_tz.localize(today_start_local).\
+            astimezone(pytz.utc).replace(tzinfo=None)
+
+        sales_today_total = model.Session.query(
+            func.sum((model.CartItem.price_each * model.CartItem.qty_desired) +
+                     model.CartItem.shipping_price)).\
+            join(model.CartItem.cart).\
+            join(model.Cart.order).\
+            filter(model.Order.created_time >= today_start_utc).\
+            scalar() or 0
+
+        orders_today_q = model.Session.query(model.Order).\
+            filter(model.Order.created_time >= today_start_utc)
+
         return {
             'crowdfunding_project_count': active_crowdfunding_q.count(),
             'available_project_count': available_q.count(),
             'order_count': order_q.count(),
             'user_count': user_q.count(),
+            'sales_today_total': sales_today_total,
+            'orders_today_count': orders_today_q.count(),
         }

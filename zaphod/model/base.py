8dbdaf5d96c6ba25de22ac6ca461ce038b78cd6d
Scott Torborg <scott@cartlogic.com>
2015-03-15 23:24:22 -0700

    Update metadata naming convention to use somewhat shorter foreign key names

--- zaphod/model/base.py
+++ zaphod/model/base.py
@@ -45,6 +45,6 @@ Base = declarative_base(cls=_Base)
 Base.metadata.naming_convention={
     "ix": 'ix_%(column_0_label)s',
     "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s",
     "pk": "pk_%(table_name)s"
 }

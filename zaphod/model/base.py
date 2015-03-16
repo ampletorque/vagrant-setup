022e7eeae3e7b99cd95a915744098219d8ad68bd
Scott Torborg <scott@cartlogic.com>
2015-03-15 23:21:08 -0700

    Add metadata naming convention for alembic convenience

--- zaphod/model/base.py
+++ zaphod/model/base.py
@@ -41,3 +41,10 @@ class _Base(object):
 
 
 Base = declarative_base(cls=_Base)
+
+Base.metadata.naming_convention={
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}

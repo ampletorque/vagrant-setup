9432c5597fc9c3fc3be7df4627d85930b1cc8fe9
Scott Torborg <scott@cartlogic.com>
2015-03-18 13:32:56 -0700

    Reindex objects when updated in BaseEditView._update_obj()

--- zaphod/admin.py
+++ zaphod/admin.py
@@ -71,6 +71,8 @@ class BaseEditView(object):
         if 'images' in form.data:
             self._handle_images(form, obj)
         form.bind(obj)
+        client = get_client(request)
+        client.index_object(obj)
         request.flash('Saved changes.', 'success')
 
     @view_config(permission='admin', xhr=False)
